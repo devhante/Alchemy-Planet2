08c021da39d4fd3241d539510b76772c4ba06640
Kyungwook Cho <amisay5256@naver.com>
2018-03-02 15:43:25 +0900

    Initialize Project, Camera Setting

---
+++ Assets/Resources/Scripts/ReSizeSpriteToCamera.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReSizeSpriteToCamera : MonoBehaviour
+{
+    private void Awake()
+    {
+        Screen.orientation = ScreenOrientation.Portrait;
+
+        Camera.main.orthographicSize = 1280 / (2 * 100f);
+
+        Screen.SetResolution(720, 1280, false);
+    }
+}
