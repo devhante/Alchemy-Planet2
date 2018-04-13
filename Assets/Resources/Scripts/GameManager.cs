f5b62a4b04b8877e75af8a95abdc03caaf8039f3
Unknown <kimjisoo690@gmail.com>
2018-04-14 01:35:17 +0900

    Add UIManager(Gages, PauseBtn)

---
+++ Assets/Resources/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    public GameManager Instance { get; private set; }
+
+    public float gameScore;
+    public float gameTime;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    IEnumerator TimeCOunt(float StartTIme)
+    {
+        gameTime = StartTIme;
+        while (gameTime < 0)
+        {
+            yield return new WaitForSeconds(1);
+            gameTime -= Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/Resources/Scripts/UIManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum Gages { Oxygen, Purify }
+
+public class UIManager : MonoBehaviour {
+    public UIManager Instance = null;
+
+    public Image OxygenGageMask;
+    public Image PurifyGageMask;
+    public Button PauseButton;
+
+    public void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+
+        PauseButton.onClick.AddListener(() => {
+            Time.timeScale = 0;
+        });
+    }
+
+    public void Start()
+    {
+        StartCoroutine("A");
+    }
+
+    public void MinusGage(Gages kind, float persent)
+    {
+        if (kind.Equals(Gages.Oxygen))
+        {
+            OxygenGageMask.transform.localScale -= new Vector3((persent / 100), 0, 0);
+        }
+        else if (kind.Equals(Gages.Purify))
+        {
+            PurifyGageMask.transform.localScale -= new Vector3((persent / 100), 0, 0);
+        }
+    }
+
+    IEnumerator A()
+    {
+        while (true)
+        {
+            MinusGage(Gages.Oxygen, 5);
+            MinusGage(Gages.Purify, 10);
+            yield return new WaitForSeconds(1);
+        }
+    }
+}
