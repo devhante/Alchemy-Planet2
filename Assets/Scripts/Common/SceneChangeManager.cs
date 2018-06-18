d954b030b63d998d3641d944999c7fdb15508255
ISHNN <ishnn032n@gmail.com>
2018-06-18 20:33:32 +0900

    Add MapScene, BottomMenu, SceneChangeManager

---
+++ Assets/Scripts/Common/SceneChangeManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChangeManager : MonoBehaviour {
+    public SceneChangeManager Instance;
+
+    public void Awake()
+    {
+        Instance = this;
+    }
+
+    public void ChangeScene(int scene_no)
+    {
+        SceneManager.LoadScene(scene_no);
+    }
+    public void ChangeScene(string scene_name)
+    {
+        SceneManager.LoadScene(scene_name);
+    }
+}
