97c21921f71bce9f4ffc97c761e565f186830c55
Kyungwook Cho <amisay5256@naver.com>
2018-06-18 23:56:26 +0900

    Add Monster Script

--- Assets/Scripts/GameScene/Character/Player.cs
+++ Assets/Scripts/GameScene/Character/Player.cs
@@ -5,5 +5,15 @@ namespace AlchemyPlanet.GameScene
     public class Player : MonoBehaviour
     {
         public static Player Instance { get; private set; }
+
+        private void OnDestroy()
+        {
+            Instance = null;
+        }
+
+        private void Awake()
+        {
+            Instance = this;
+        }
     }
 }
