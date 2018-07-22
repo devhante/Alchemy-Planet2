967d1f0de2bcbfd138add65fb731d065d4f44154
SCOUTgit <k01095232802@gmail.com>
2018-07-22 13:14:19 +0900

    캐릭터 대화중복오류 해결

--- Assets/Scripts/TownScene/NPC.cs
+++ Assets/Scripts/TownScene/NPC.cs
@@ -2,94 +2,111 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NPC : MonoBehaviour {
-
-    public float speed;
-    public float moveTime;
-
-    private int moveChoice;         // 움직임
-    private bool moving;            // 움직이는중
-    
-	// Use this for initialization
-	void Start () {
-        moving = false;
-        moveChoice = Random.Range(0, 3);
-    }
-	
-	// Update is called once per frame
-	void Update () {
-        Move();
-	}
-
-    private void Move()
+namespace AlchemyPlanet.TownScene
+{
+    public class NPC : MonoBehaviour
     {
-        if (!moving)
+
+        public float speed;
+        public float moveTime;
+
+        private int moveChoice;         // 움직임
+        private bool moving;            // 움직이는 중
+        private bool talking = false;           // 말하는 중
+
+        // Use this for initialization
+        void Start()
         {
-            switch (moveChoice)
+            moving = false;
+            moveChoice = Random.Range(0, 3);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Move();
+        }
+
+        private void Move()
+        {
+            if (!moving)
             {
-                case 0:
-                    StartCoroutine("LeftMove");
-                    break;
-                case 1:
-                    StartCoroutine("RightMove");
-                    break;
-                case 2:
-                    StartCoroutine("StopMove");
-                    break;
+                switch (moveChoice)
+                {
+                    case 0:
+                        StartCoroutine("LeftMove");
+                        break;
+                    case 1:
+                        StartCoroutine("RightMove");
+                        break;
+                    case 2:
+                        StartCoroutine("StopMove");
+                        break;
+                }
             }
         }
-    }
-    
-    void stop()
-    {
-        if (moving)
+
+        void stop()
         {
-            switch (moveChoice)
+            if (moving)
             {
-                case 0:
-                    StopCoroutine("LeftMove");
-                    break;
-                case 1:
-                    StopCoroutine("RightMove");
-                    break;
+                switch (moveChoice)
+                {
+                    case 0:
+                        StopCoroutine("LeftMove");
+                        break;
+                    case 1:
+                        StopCoroutine("RightMove");
+                        break;
+                }
             }
         }
-    }
 
-    void remove() { moving = false; }
-
-    private IEnumerator LeftMove()
-    {
-        moving = true;
-        for (int i=0; i<moveTime; i++)
+        void talk()
         {
-            transform.Translate(Vector2.left * speed * Time.deltaTime);
-            yield return new WaitForFixedUpdate();
-        }
-        moving = false;
-        moveChoice = Random.Range(1, 3);
-        yield return null;
-    }
-    
-    private IEnumerator RightMove()
-    {
-        moving = true;
-        for (int i = 0; i < moveTime; i++)
-        {
-            transform.Translate(Vector2.right * speed * Time.deltaTime);
-            yield return new WaitForFixedUpdate();
-        }
-        moving = false;
-        moveChoice = Random.Range(0, 2)==1?2:0;
-        yield return null;
-    }
+            if (!talking)
+            {
+                talking = true;
+                UIManager.Instance.OpenMenu<DialogUI>();
+                talking = false;
+            }
 
-    private IEnumerator StopMove()
-    {
-        moving = true;
-        yield return new WaitForSeconds(2f);
-        moving = false;
-        moveChoice = Random.Range(0, 2);
-        yield return null;
+            moving = false;
+        }
+
+        private IEnumerator LeftMove()
+        {
+            moving = true;
+            for (int i = 0; i < moveTime; i++)
+            {
+                transform.Translate(Vector2.left * speed * Time.deltaTime);
+                yield return new WaitForFixedUpdate();
+            }
+            moving = false;
+            moveChoice = Random.Range(1, 3);
+            yield return null;
+        }
+
+        private IEnumerator RightMove()
+        {
+            moving = true;
+            for (int i = 0; i < moveTime; i++)
+            {
+                transform.Translate(Vector2.right * speed * Time.deltaTime);
+                yield return new WaitForFixedUpdate();
+            }
+            moving = false;
+            moveChoice = Random.Range(0, 2) == 1 ? 2 : 0;
+            yield return null;
+        }
+
+        private IEnumerator StopMove()
+        {
+            moving = true;
+            yield return new WaitForSeconds(2f);
+            moving = false;
+            moveChoice = Random.Range(0, 2);
+            yield return null;
+        }
     }
 }
--- Assets/Scripts/TownScene/Player.cs
+++ Assets/Scripts/TownScene/Player.cs
@@ -3,95 +3,94 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-namespace AlchemyPlanet.TownScene
+public class Player : MonoBehaviour
 {
-    public class Player : MonoBehaviour {
 
-        public float speed;         // 속도
+    public float speed;         // 속도
 
-        private Touch tempTouch;   // 터치들
-        private Vector3 touchedPos; // 터치위치
-        private Animator animator;  // 애니메이터
+    private Touch tempTouch;   // 터치들
+    private Vector3 touchedPos; // 터치위치
+    private Animator animator;  // 애니메이터
 
-        // Use this for initialization
-        void Start() {
-            animator = GetComponent<Animator>();
-        }
+    // Use this for initialization
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
 
-        // Update is called once per frame
-        void Update() {
-            DetectTouch();
-            DetectClick();
-        }
-        
-        void DetectTouch()    // 터치감지
+    // Update is called once per frame
+    void Update()
+    {
+        DetectTouch();
+        DetectClick();
+    }
+
+    void DetectTouch()    // 터치감지
+    {
+        if (Input.touchCount > 0)
         {
-            if (Input.touchCount > 0)
+            tempTouch = Input.GetTouch(0);
+            if (tempTouch.phase == TouchPhase.Began && EventSystem.current.IsPointerOverGameObject() == false)
             {
-                tempTouch = Input.GetTouch(0);
-                if (tempTouch.phase == TouchPhase.Began && EventSystem.current.IsPointerOverGameObject() == false)
-                {
-                    touchedPos = Camera.main.ScreenToWorldPoint(tempTouch.position);
-                    RaycastHit2D hit = Physics2D.Raycast(touchedPos, Vector2.zero);
-                    if (hit.collider.tag == "Building" || hit.collider.tag == "Road" || hit.collider.tag == "NPC")
-                    {
-                        StopCoroutine("Move");
-                        StartCoroutine("Move", hit.collider.gameObject);
-                    }
-                }
-            }
-        }
-
-        void DetectClick()    // 클릭감지
-        {
-            if (Input.GetMouseButtonDown(0))
-            {
-                touchedPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                touchedPos = Camera.main.ScreenToWorldPoint(tempTouch.position);
                 RaycastHit2D hit = Physics2D.Raycast(touchedPos, Vector2.zero);
-                if (hit && EventSystem.current.IsPointerOverGameObject() == false)
+                if (hit.collider.tag == "Building" || hit.collider.tag == "Road" || hit.collider.tag == "NPC")
                 {
-                    if (hit.collider.tag == "Road" || hit.collider.tag == "NPC" || hit.collider.tag == "Building")
-                    {
-                        StopCoroutine("Move");
-                        StartCoroutine("Move", hit.collider.gameObject);
-                    }
+                    StopCoroutine("Move");
+                    StartCoroutine("Move", hit.collider.gameObject);
                 }
             }
         }
+    }
 
-        IEnumerator Move(GameObject obj)    //캐릭터 움직이기
+    void DetectClick()    // 클릭감지
+    {
+        if (Input.GetMouseButtonDown(0))
         {
-            animator.SetBool("Run", true);
-            Debug.Log(transform.position.x - obj.transform.position.x);
-            if (transform.position.x - obj.transform.position.x < 0)
+            touchedPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(touchedPos, Vector2.zero);
+            if (hit && EventSystem.current.IsPointerOverGameObject() == false)
             {
-                transform.rotation = Quaternion.Euler(0, 0, 0);
-            }
-            else if (transform.position.x - obj.transform.position.x > 0)
-            {
-                transform.rotation = Quaternion.Euler(0, 180, 0);
-            }
-            if (obj.tag == "NPC")
-            {
-                obj.SendMessage("stop");
-                while (transform.position.x - obj.transform.position.x > 1.5f || transform.position.x - obj.transform.position.x < -1.5f)
+                if (hit.collider.tag == "Road" || hit.collider.tag == "NPC" || hit.collider.tag == "Building")
                 {
-                    transform.Translate(Vector2.right * speed * Time.deltaTime);
-                    yield return new WaitForFixedUpdate();
-                }
-                UIManager.Instance.OpenMenu<DialogUI>();
-                obj.SendMessage("remove");
-            }
-            else
-            {
-                while (transform.position.x - obj.transform.position.x > 0.1f || transform.position.x - obj.transform.position.x < -0.1f)
-                {
-                    transform.Translate(Vector2.right * speed * Time.deltaTime);
-                    yield return new WaitForFixedUpdate();
+                    StopCoroutine("Move");
+                    StartCoroutine("Move", hit.collider.gameObject);
                 }
             }
-            animator.SetBool("Run", false);
-            yield return null;
         }
+    }
+
+    IEnumerator Move(GameObject obj)    //캐릭터 움직이기
+    {
+        animator.SetBool("Run", true);
+        Debug.Log(transform.position.x - obj.transform.position.x);
+        if (transform.position.x - obj.transform.position.x < 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+        else if (transform.position.x - obj.transform.position.x > 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+        if (obj.tag == "NPC")
+        {
+            obj.SendMessage("stop");
+            while (transform.position.x - obj.transform.position.x > 1.5f || transform.position.x - obj.transform.position.x < -1.5f)
+            {
+                transform.Translate(Vector2.right * speed * Time.deltaTime);
+                yield return new WaitForFixedUpdate();
+            }
+            obj.SendMessage("talk");
+        }
+        else
+        {
+            while (transform.position.x - obj.transform.position.x > 0.1f || transform.position.x - obj.transform.position.x < -0.1f)
+            {
+                transform.Translate(Vector2.right * speed * Time.deltaTime);
+                yield return new WaitForFixedUpdate();
+            }
+        }
+        animator.SetBool("Run", false);
+        yield return null;
     }
 }
