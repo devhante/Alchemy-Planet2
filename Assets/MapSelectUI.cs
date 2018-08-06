d8f0112cd4352cee598ef829dd91c4d48d539275
Unknown <ishnn032n@gmail.com>
2018-08-07 08:40:21 +0900

    Play Map 선택 씬 구현

---
+++ Assets/MapSelectUI.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapSelectUI : MonoBehaviour {
+    public void SetStageIndex(int index)
+    {
+        DataManager.Instance.selected_stage = index;
+    }
+}
