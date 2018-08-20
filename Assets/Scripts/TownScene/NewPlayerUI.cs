7bc8bc16c566e3443c9f4ba0064e4e15b80b539b
Unknown <ishnn032n@gmail.com>
2018-08-20 17:11:45 +0900

    플레이어 이름 적용 UI 추가

---
+++ Assets/Scripts/TownScene/NewPlayerUI.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace AlchemyPlanet.TownScene
+{
+    public class NewPlayerUI : Common.UI
+    {
+        [SerializeField] private InputField PlayerNameInput;
+        [SerializeField] private Button CommitButton;
+        private void Awake()
+        {
+            CommitButton.onClick.AddListener(() => {
+                Data.DataManager.Instance.CurrentPlayerData.player_name = PlayerNameInput.text;
+            });
+        }
+    }
+
+}
