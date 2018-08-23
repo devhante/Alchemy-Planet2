61f8f4114850ea303736916777e3f802d6222871
Unknown <ishnn032n@gmail.com>
2018-08-23 16:32:05 +0900

    PlanetSelect 에 DoTween적용

---
+++ Assets/Scripts/Common/PlanetSelectUI.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+public class PlanetSelectUI : MonoBehaviour {
+    [SerializeField] private GameObject Planets;
+	void Start () {
+        Planets.transform.DOMoveX(Screen.width / 2, 1).SetEase(Ease.InSine);
+    }
+
+    public void MoveDown()
+    {
+        Planets.transform.DOMoveY(- Screen.height / 2, 1).SetEase(Ease.InSine);
+    }
+}
