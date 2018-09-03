37abac5d3c068a641dedcfb221d031fb94e5894b
Unknown <ishnn032n@gmail.com>
2018-09-03 11:32:43 +0900

    PlanetUI Animation 수정

--- Assets/Scripts/Common/PlanetSelectUI.cs
+++ Assets/Scripts/Common/PlanetSelectUI.cs
@@ -4,13 +4,15 @@ using DG.Tweening;
 namespace AlchemyPlanet.Common {
     public class PlanetSelectUI : MonoBehaviour
     {
+        [SerializeField] private Camera mainCam;
         [SerializeField] private GameObject Planets;
         [SerializeField] private GameObject AdventurePlanetGroup;
         private bool played;
         void Start()
         {
+            mainCam.transform.DOMoveX(5,1).SetEase(Ease.OutQuart);
             AdventurePlanetGroup.transform.position = new Vector2(Planets.transform.position.x, Screen.height * 1.5f);
-            Planets.transform.DOMoveX(Screen.width / 2, 1).SetEase(Ease.InSine);
+            Planets.transform.DOMoveX(Screen.width / 2, 1).SetEase(Ease.OutQuart);
         }
 
         public void MoveDown()
@@ -18,7 +20,8 @@ namespace AlchemyPlanet.Common {
             if (!played)
             {
                 played = true;
-                Planets.transform.DOMoveY(-Screen.height / 2, 1).SetEase(Ease.InSine);
+                mainCam.transform.DOMoveY(2, 1).SetEase(Ease.OutQuart);
+                Planets.transform.DOMoveY(-Screen.height / 2, 1).SetEase(Ease.OutQuart);
             }
         }
 
