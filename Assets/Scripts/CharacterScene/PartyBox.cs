1307562a501fcd22c32bcf7ad8b5b519d9138b63
Kyungwook Cho <amisay5256@naver.com>
2018-09-09 06:31:24 +0900

    캐릭터 씬 파티 박스 드래그로 좌우 움직임 가능하게 구현

---
+++ Assets/Scripts/CharacterScene/PartyBox.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace AlchemyPlanet.CharacterScene
+{
+    public class PartyBox : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+    {
+        float originPositionX;
+        float clickedPositionX;
+
+        private void Start()
+        {
+            originPositionX = PartyUI.Instance.partyBoxPosition.transform.position.x;
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            clickedPositionX = Input.mousePosition.x;
+            StartCoroutine("MoveCoroutine");
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            StopCoroutine("MoveCoroutine");
+
+            float distance = Input.mousePosition.x - clickedPositionX;
+
+            if (distance >= 100)
+                PartyUI.Instance.OnClickButtonRight();
+            else if (distance <= -100)
+                PartyUI.Instance.OnClickButtonLeft();
+            else
+                transform.position = new Vector3(originPositionX, transform.position.y, transform.position.z);
+        }
+
+        IEnumerator MoveCoroutine()
+        {
+            while(true)
+            {
+                float posX = originPositionX + (Input.mousePosition.x - clickedPositionX);
+                transform.position = new Vector3(posX, transform.position.y, transform.position.z);
+                yield return null;
+            }
+        }
+    }
+}
