8af4e642785ab85f44decbcd9757c59ed41763b9
Unknown <ishnn032n@gmail.com>
2018-09-11 16:23:15 +0900

    요청UI추가

---
+++ Assets/Scripts/AlchemyScene/RequestUI.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace AlchemyPlanet.AlchemyScene
+{
+    public class RequestUI : Common.UI<RequestUI>
+    {
+        [SerializeField] private Button[] RequestButtons = new Button[4];
+        //의뢰 리스트
+
+        [SerializeField] private Button BackButton;
+        [SerializeField] private Button UpdateRequestButton;
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            foreach (var item in RequestButtons)
+                item.onClick.AddListener(OnClickRequestButton);
+
+            BackButton.onClick.AddListener(OnClickBackButton);
+
+            UpdateRequestButton.onClick.AddListener(OnClickUpdateRequestButton);
+        }
+
+        private void OnClickRequestButton()
+        {
+
+        }
+
+        private void OnClickBackButton()
+        {
+
+        }
+
+        private void OnClickUpdateRequestButton()
+        {
+
+        }
+    }
+}
