b69cee9930e95a95e1f6ed977cc59b3f3fae0e86
Kyungwook Cho <amisay5256@naver.com>
2018-09-11 23:31:55 +0900

    WebSocket 추가

---
+++ Assets/Scripts/TownScene/StructureInfo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AlchemyPlanet.TownScene
+{
+    public class GetStructureUpgradeInfo
+    {
+        public string type = "GetStructureUpgradeInfo";
+        public int userId;
+        public int structureId;
+
+        public GetStructureUpgradeInfo(int uid, int sid)
+        {
+            userId = uid;
+            structureId = sid;
+        }
+    }
+
+    public class SetStructureUpgradeInfo
+    {
+        public string type = "SetStructureUpgradeInfo";
+        public int userId;
+        public int structureId;
+        public DateTime startDate;
+        public int requireTime;
+
+        public SetStructureUpgradeInfo(int uid, int sid, DateTime sdate, int rtime)
+        {
+            userId = uid;
+            structureId = sid;
+            startDate = sdate;
+            requireTime = rtime;
+        }
+    }
+}
