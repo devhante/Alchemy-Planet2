6cea3e56a566581ffc86cf915437c57aeeb9aa1b
Kyungwook Cho <amisay5256@naver.com>
2018-09-13 10:28:40 +0900

    ShopUI 구성

---
+++ Assets/Scripts/ShopScene/Tag.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace AlchemyPlanet.ShopScene
+{
+    public class Tag : MonoBehaviour
+    {
+        private Image image;
+        private Button button;
+
+        private void Awake()
+        {
+            image = GetComponent<Image>();
+            button = GetComponent<Button>();
+            button.onClick.AddListener(OnClickTag);
+        }
+
+        private void OnClickTag()
+        {
+            if(ShopUI.Instance.selectedTag != image)
+            {
+                ShopUI.Instance.DeselectTag();
+                ShopUI.Instance.SelectTag(image);
+            }
+        }
+    }
+}
