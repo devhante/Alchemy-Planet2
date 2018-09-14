8e4df496ca1667eb47780ddfd6418af3498914c3
Kyungwook Cho <amisay5256@naver.com>
2018-09-14 13:47:07 +0900

    웹소켓 예제 소스 재구성

---
+++ Assets/Scripts/Data/Message.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AlchemyPlanet.Data
+{
+    public class Message
+    {
+        public string status { get; set; }
+        public string data { get; set; }
+
+        public Message(string status, string data)
+        {
+            this.status = status;
+            this.data = data;
+        }
+
+        public Message(string status, object data)
+        {
+            this.status = status;
+            this.data = JsonConvert.SerializeObject(data);
+        }
+    }
+}
