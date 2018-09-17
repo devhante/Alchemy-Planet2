766bac366549c81ff3712e3fdef3ae3ec27144e9
Unknown <ishnn032n@gmail.com>
2018-09-17 19:51:45 +0900

    상단UI 닉네임, 레벨 , 경험치 바 적용

--- Assets/Scripts/Common/StateBar.cs
+++ Assets/Scripts/Common/StateBar.cs
@@ -8,6 +8,10 @@ namespace AlchemyPlanet.Common
     {
         public static StateBar Instance;
 
+        [SerializeField] private Text playerName;
+        [SerializeField] private Text playerLevel;
+        [SerializeField] private Image Expbar;
+
         [SerializeField] private Text Unicoin;
         [SerializeField] private Text Cosmoston;
         [SerializeField] private Text OxygenTank;
@@ -29,8 +33,13 @@ namespace AlchemyPlanet.Common
 
         public void UpdateState()
         {
+            playerName.text = Data.DataManager.Instance.CurrentPlayerData.player_name.ToString();
+            playerLevel.text = Data.DataManager.Instance.CurrentPlayerData.level.ToString();
+
+            Expbar.fillAmount = Data.DataManager.Instance.CurrentPlayerData.exp / System.Int32.Parse(playerLevel.text);
+
             Unicoin.text = Data.DataManager.Instance.CurrentPlayerData.unicoin.ToString();
-            Cosmoston.text = Data.DataManager.Instance.CurrentPlayerData.cosmoston.ToString();
+            Cosmoston.text = Data.DataManager.Instance.CurrentPlayerData.cosmostone.ToString();
             OxygenTank.text =
                Data.DataManager.Instance.CurrentPlayerData.oxygentank.ToString() + "/" +
                Data.DataManager.Instance.CurrentPlayerData.Max_oxygentank.ToString();
