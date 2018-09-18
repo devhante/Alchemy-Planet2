0f79721e1578db7bd90e274a960990741c7bc017
Kyungwook Cho <amisay5256@naver.com>
2018-09-18 16:00:20 +0900

    Character 씬 캐릭터 & 서버 연결 준비

---
+++ Assets/Scripts/CharacterScene/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AlchemyPlanet.CharacterScene
+{
+    public class GameManager : MonoBehaviour
+    {
+        public static GameManager Instance { get; set; }
+
+        public Data.Character[,] Parties { get; set; } 
+
+        private void OnDestroy()
+        {
+            Instance = null;
+        }
+
+        private void Awake()
+        {
+            Instance = this;
+            Parties = new Data.Character[9, 3];
+        }
+    }
+}
