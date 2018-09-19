cecd8eee5048c4ad76935289ea7f42d4a876ce68
Unknown <ishnn032n@gmail.com>
2018-09-19 11:23:36 +0900

    카메라 페이드 인 제작

--- Assets/Scripts/PrologueScene/MainCamera.cs
+++ Assets/Scripts/PrologueScene/MainCamera.cs
@@ -1,16 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
 
 namespace AlchemyPlanet.PrologueScene
 {
     public class MainCamera : MonoBehaviour
     {
         [SerializeField] private GameObject player;
+        [SerializeField] private Image fade;
 
         private void Start()
         {
-
+            fade.DOFade(0, 2).SetEase(Ease.InBack).OnComplete(() => fade.gameObject.SetActive(false));
         }
 
         void Update()
