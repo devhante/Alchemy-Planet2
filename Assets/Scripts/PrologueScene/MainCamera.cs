6cc6c4380bfce44f6cacb85ea55520b85f67e63b
Unknown <ishnn032n@gmail.com>
2018-09-19 17:23:18 +0900

    프롤로그 동작 구현(미완)

--- Assets/Scripts/PrologueScene/MainCamera.cs
+++ Assets/Scripts/PrologueScene/MainCamera.cs
@@ -11,14 +11,38 @@ namespace AlchemyPlanet.PrologueScene
         [SerializeField] private GameObject player;
         [SerializeField] private Image fade;
 
+        private Vector3 offset;
+
+        private WaitForEndOfFrame frame = new WaitForEndOfFrame();
+
+        private void Awake()
+        {
+            offset = this.transform.position - player.transform.position;
+        }
+
         private void Start()
+        {
+            StartCoroutine(Follow());
+        }
+
+        public void FadeIn()
         {
             fade.DOFade(0, 2).SetEase(Ease.InBack).OnComplete(() => fade.gameObject.SetActive(false));
         }
 
-        void Update()
+        public void FadeOut()
         {
+            fade.gameObject.SetActive(true);
+            fade.DOFade(1, 2).SetEase(Ease.InBack);
+        }
 
+        IEnumerator Follow()
+        {
+            while (true)
+            {
+                this.gameObject.transform.position = player.transform.position + offset;
+                yield return frame;
+            }
         }
     }
 }
