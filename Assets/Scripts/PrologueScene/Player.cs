f7afe7b8cd8048512af8cfccd45b51d045aaaf69
Unknown <ishnn032n@gmail.com>
2018-09-20 10:50:59 +0900

    프롤로그 오브젝트 배치

---
+++ Assets/Scripts/PrologueScene/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AlchemyPlanet.PrologueScene
+{
+    public class Player : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/PrologueScene/PrologueObject.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+namespace AlchemyPlanet.PrologueScene
+{
+    public enum ObjectKind { NPC, Object, Door }
+
+    public class PrologueObject : MonoBehaviour
+    {
+        [SerializeField] private string objcect_name;
+        [SerializeField] private ObjectKind kind;
+
+        [SerializeField] private GameObject bubble;
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.CompareTag("Player"))
+            {
+                Debug.Log("On");
+                switch (kind)
+                {
+                    case ObjectKind.NPC:
+                        {
+                            OpenDialog();
+                            break;
+                        }
+                    case ObjectKind.Object:
+                        {
+                            ShowBubble();
+                            break;
+                        }
+                    case ObjectKind.Door:
+                        {
+                            MoveNext();
+                            break;
+                        }
+                }
+            }
+        }
+
+        public void OpenDialog()
+        {
+            Data.DataManager.Instance.selected_dialog = new Data.NPCDAta(objcect_name);
+            TownScene.UIManager.Instance.OpenMenu<TownScene.DialogUI>();
+            TownScene.DialogUI.Instance.SetDialog();
+        }
+
+        public void ShowBubble()
+        {
+            bubble.transform.DOScale(1, 0.5f);
+        }
+        public void MoveNext()
+        {
+
+        }
+    }
+}
