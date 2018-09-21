8a16a5bad538802633c5fe536fec9bee9ca98fba
Kyungwook Cho <amisay5256@naver.com>
2018-09-21 16:32:18 +0900

    스토리 로비 진입가능, 스토리/캐릭터 UI 거의 완성

---
+++ Assets/Scripts/StoryLobbyScene/StoryLobbyUI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StoryLobbyUI : MonoBehaviour
+{
+    public Button starButton;
+    public Button starDescriptionButton;
+
+    private void Awake()
+    {
+        starButton.onClick.AddListener(OnClickStarButton);
+        starDescriptionButton.onClick.AddListener(OnClickStarDescriptionButton);
+    }
+
+    private void OnClickStarButton()
+    {
+        starDescriptionButton.gameObject.SetActive(!starDescriptionButton.gameObject.activeSelf);
+        Debug.Log("누름");
+    }
+
+    private void OnClickStarDescriptionButton()
+    {
+        starDescriptionButton.gameObject.SetActive(false);
+    }
+}
