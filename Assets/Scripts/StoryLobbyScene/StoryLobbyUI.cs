7141adb580d2c3afcd4271131d8ff12aad71bf23
Kyungwook Cho <amisay5256@naver.com>
2018-09-22 18:50:06 +0900

    StoryLobby 완료

--- Assets/Scripts/StoryLobbyScene/StoryLobbyUI.cs
+++ Assets/Scripts/StoryLobbyScene/StoryLobbyUI.cs
@@ -12,9 +12,22 @@ namespace AlchemyPlanet.StoryLobbyScene
         public Button starDescriptionButton;
         public Image[] starImages;
         public Image[] starDescriptionImages;
+        public Text stageText;
+        public GameObject stageCircles;
+        public GameObject stageCircle;
+        public GameObject stageBoss;
+        public Button buttonLeft;
+        public Button buttonRight;
 
         public Sprite starOff;
         public Sprite starOn;
+        public Sprite stageCircleWhite;
+        public Sprite stageCircleBlack;
+        public Sprite stageCircleYellow;
+
+        public int CurrentChaper { get; set; }
+        public int CurrentStage { get; set; }
+        public int CurrentMaxStage { get; set; }
 
         private List<StoryChallengeData> list;
 
@@ -23,16 +36,21 @@ namespace AlchemyPlanet.StoryLobbyScene
             starButton.onClick.AddListener(OnClickStarButton);
             starDescriptionButton.onClick.AddListener(OnClickStarDescriptionButton);
 
+            CurrentChaper = 1;
+            CurrentMaxStage = 7;
             list = DataManager.Instance.LoadStoryChallenges();
         }
 
         private void Start()
         {
+            InstantiateStageCircles(CurrentMaxStage);
+
             for (int i = 0; i < 3; i++)
             {
+                stageText.text = string.Format("STAGE {0}-{1}", CurrentChaper.ToString(), CurrentStage.ToString());
                 starDescriptionButton.transform.GetChild(1).GetChild(i).GetComponent<Text>().text = list[0].challenges[i];
 
-                if (DataManager.Instance.CurrentPlayerData.stroystar["1-1"] > i)
+                if (DataManager.Instance.CurrentPlayerData.stroystar[CurrentChaper.ToString() + "-" + CurrentStage.ToString()] > i)
                 {
                     starImages[i].sprite = starOn;
                     starDescriptionImages[i].sprite = starOn;
@@ -53,6 +71,55 @@ namespace AlchemyPlanet.StoryLobbyScene
         private void OnClickStarDescriptionButton()
         {
             starDescriptionButton.gameObject.SetActive(false);
+        }
+
+        private void OnClickButtonLeft()
+        {
+            if (CurrentStage == 1) return;
+            if (CurrentStage != CurrentMaxStage)
+                stageCircles.transform.GetChild(CurrentStage - 1).GetComponent<Image>().sprite = stageCircleWhite;
+            stageCircles.transform.GetChild(CurrentStage - 1).GetComponent<Image>().sprite = stageCircleYellow;
+            CurrentStage--;
+            stageText.text = string.Format("STAGE {0}-{1}", CurrentChaper.ToString(), CurrentStage.ToString());
+        }
+
+        private void OnClickButtonRight()
+        {
+            if (CurrentStage == CurrentMaxStage) return;
+            if (stageCircles.transform.GetChild(CurrentStage).GetComponent<Image>().sprite == stageCircleBlack) return;
+            if (CurrentStage != CurrentMaxStage - 1)
+                stageCircles.transform.GetChild(CurrentStage).GetComponent<Image>().sprite = stageCircleYellow;
+            stageCircles.transform.GetChild(CurrentStage - 1).GetComponent<Image>().sprite = stageCircleWhite;
+            CurrentStage++;
+            stageText.text = string.Format("STAGE {0}-{1}", CurrentChaper.ToString(), CurrentStage.ToString());
+        }
+
+        private void InstantiateStageCircles(int stageNumber)
+        {
+            float distance = 400.0f / (stageNumber - 1);
+            for(int i = 0; i < stageNumber; i++)
+            {
+                GameObject instance;
+                if (i == stageNumber - 1)
+                    instance = Instantiate(stageBoss, stageCircles.transform);
+                else
+                    instance = Instantiate(stageCircle, stageCircles.transform);
+
+                instance.transform.position = stageCircles.transform.position + new Vector3(-200 + (distance * i), 0);
+            }
+
+            bool isCleared = true;
+            for(int i = 1; i < stageNumber; i++)
+            {
+                if (isCleared == false)
+                    stageCircles.transform.GetChild(i - 1).GetComponent<Image>().sprite = stageCircleBlack;
+                else if (DataManager.Instance.CurrentPlayerData.stroystar[CurrentChaper.ToString() + "-" + i.ToString()] == 0)
+                {
+                    stageCircles.transform.GetChild(i - 1).GetComponent<Image>().sprite = stageCircleYellow;
+                    CurrentStage = i;
+                    isCleared = false;
+                }
+            }
         }
     }
 }
---
+++ Assets/Scripts/StoryLobbyScene/StoryPartyUI.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+namespace AlchemyPlanet.StoryLobbyScene
+{
+    public class StoryPartyUI : MonoBehaviour
+    {
+        public static StoryPartyUI Instance { get; private set; }
+
+        public Button buttonLeft;
+        public Button buttonRight;
+        public GameObject partyBoxPosition;
+        public GameObject partyBoxPrefab;
+        public Sprite[] characterProfiles;
+
+        private GameObject partyBoxObject;
+        public int PartyIndex { get; set; }
+
+        private void OnDestroy()
+        {
+            Instance = null;
+        }
+
+        private void Awake()
+        {
+            Instance = this;
+            PartyIndex = 1;
+
+            partyBoxObject = Instantiate(partyBoxPrefab, partyBoxPosition.transform.position, Quaternion.identity, transform);
+            buttonLeft.onClick.AddListener(OnClickButtonLeft);
+            buttonRight.onClick.AddListener(OnClickButtonRight);
+        }
+
+        public void OnClickButtonLeft()
+        {
+            buttonLeft.gameObject.SetActive(false);
+            buttonRight.gameObject.SetActive(false);
+
+            Sequence sq1 = DOTween.Sequence();
+            sq1.Append(partyBoxObject.transform.DOMoveX(partyBoxObject.transform.position.x - 700, 0.5f)
+                .SetEase(Ease.OutQuint));
+
+            GameObject newObject = Instantiate(partyBoxPrefab, partyBoxPosition.transform.position + new Vector3(700, 0, 0), Quaternion.identity, transform);
+            Sequence sq2 = DOTween.Sequence();
+            sq2.Append(newObject.transform.DOMoveX(newObject.transform.position.x - 700, 0.5f)
+                .SetEase(Ease.OutQuint)
+                .OnComplete(() => {
+                    Destroy(partyBoxObject);
+                    partyBoxObject = newObject;
+                    buttonLeft.gameObject.SetActive(true);
+                    buttonRight.gameObject.SetActive(true);
+                }));
+
+
+            if (PartyIndex == 1) PartyIndex = 9;
+            else PartyIndex--;
+        }
+
+        public void OnClickButtonRight()
+        {
+            buttonLeft.gameObject.SetActive(false);
+            buttonRight.gameObject.SetActive(false);
+
+            Sequence sq = DOTween.Sequence();
+            sq.Append(partyBoxObject.transform.DOMoveX(partyBoxObject.transform.position.x + 700, 0.5f)
+                .SetEase(Ease.OutQuint));
+
+            GameObject newObject = Instantiate(partyBoxPrefab, partyBoxPosition.transform.position + new Vector3(-700, 0, 0), Quaternion.identity, transform);
+            Sequence sq2 = DOTween.Sequence();
+            sq2.Append(newObject.transform.DOMoveX(newObject.transform.position.x + 700, 0.5f)
+                .SetEase(Ease.OutQuint)
+                .OnComplete(() => {
+                    Destroy(partyBoxObject);
+                    partyBoxObject = newObject;
+                    buttonLeft.gameObject.SetActive(true);
+                    buttonRight.gameObject.SetActive(true);
+                }));
+
+            if (PartyIndex == 9) PartyIndex = 1;
+            else PartyIndex++;
+        }
+    }
+}
