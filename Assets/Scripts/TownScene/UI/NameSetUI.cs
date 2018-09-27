67862ffb24809407131f52e59e45636339b33a4e
Kyungwook Cho <amisay5256@naver.com>
2018-09-27 19:18:30 +0900

    storystar 저장 오류 해결

--- Assets/Scripts/TownScene/UI/NameSetUI.cs
+++ Assets/Scripts/TownScene/UI/NameSetUI.cs
@@ -20,7 +20,7 @@ namespace AlchemyPlanet.TownScene
                 DataManager.Instance.CurrentPlayerData.party[0, 0] = CharacterEnum.Popin;
                 WebSocketManager.Instance.SendInsertParty("0", DataManager.Instance.CurrentPlayerData.player_id, 1, 1, "0");
 
-                DataManager.Instance.CurrentPlayerData.stroystar["1-1"] = 3;
+                DataManager.Instance.CurrentPlayerData.stroystar["1-1"] = 0;
                 WebSocketManager.Instance.SendInsertStoryStar("0", DataManager.Instance.CurrentPlayerData.player_id, "1-1", 0);
 
                 UIManager.Instance.CloseMenu();
