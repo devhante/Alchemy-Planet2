b85fbd59ab4c69d4be3093584f82434051b2cd97
Unknown <ishnn032n@gmail.com>
2018-09-27 15:51:47 +0900

    SoundManage 대화, 로딩 동작 구현

---
+++ Assets/Scripts/Common/SoundManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AlchemyPlanet.Common
+{
+    public class SoundManager : MonoBehaviour
+    {
+        public static SoundManager Instance;
+
+        [SerializeField] private AudioClip[] bgms;
+        private AudioSource AudioPlayer;
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else if (Instance != this)
+            {
+                Destroy(this.gameObject);
+            }
+
+            AudioPlayer = GetComponent<AudioSource>();
+        }
+
+        public void PlayBGM(int no)
+        {
+            StopBGM();
+
+            AudioPlayer.clip = bgms[no];
+            AudioPlayer.Play();
+        }
+        public void PauseCurrentBGM()
+        {
+            if (AudioPlayer.isPlaying)
+                AudioPlayer.Pause();
+        }
+        public void ResumeCurrentBGM()
+        {
+            if (AudioPlayer.isPlaying)
+                AudioPlayer.UnPause();
+        }
+        public void StopBGM()
+        {
+            if (AudioPlayer.isPlaying)
+                AudioPlayer.Stop();
+        }
+    }
+}
