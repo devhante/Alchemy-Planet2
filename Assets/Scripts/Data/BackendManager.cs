a671713fa073a2f2e0136e8272323c015ff5bbfc
Kyungwook Cho <amisay5256@naver.com>
2019-05-03 00:32:09 +0900

    [Add] Town Table Insert, Update, Delete 함수 작성

--- Assets/Scripts/Data/BackendManager.cs
+++ Assets/Scripts/Data/BackendManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using BackEnd;
 using LitJson;
+using System;
 
 public class BackendManager : MonoBehaviour
 {
@@ -83,9 +84,7 @@ public class BackendManager : MonoBehaviour
         param.Add("cosmoStone", 0);
         param.Add("oxygenTank", 0);
 
-        BackendReturnObject bro = Backend.GameInfo.Insert("player", param);
-        if (bro.IsSuccess()) Debug.Log("Insert 성공");
-        else Debug.Log("Insert 실패");
+        Backend.GameInfo.Insert("player", param);
     }
 
     public void TestInsertPlayer()
@@ -102,9 +101,7 @@ public class BackendManager : MonoBehaviour
         param.Add("cosmoStone", 0);
         param.Add("oxygenTank", 0);
 
-        BackendReturnObject bro = Backend.GameInfo.Insert("player", param);
-        if (bro.IsSuccess()) Debug.Log("Insert 성공");
-        else Debug.Log("Insert 실패");
+        Backend.GameInfo.Insert("player", param);
     }
 
     private void UpdatePlayerName(string inDate, string name)
@@ -162,37 +159,38 @@ public class BackendManager : MonoBehaviour
     {
         Param param = new Param();
         param.Add("playerId", playerId);
-        Dictionary<string, int> dict = new Dictionary<string, int>();
-        param.Add("itemDict", dict);
 
-        BackendReturnObject bro = Backend.GameInfo.Insert("item", param);
+        Dictionary<string, int> dict = new Dictionary<string, int>();
+        param.Add("items", dict);
+
+        Backend.GameInfo.Insert("item", param);
     }
 
-    private void DestroyItem(string inDate, string itemName)
+    private void DestroyItem(string inDate)
     {
         Backend.GameInfo.Delete("item", inDate);
     }
 
     private void UpdateItem(string inDate, string itemName, int number)
     {
-        JsonData jsonData = Backend.GameInfo.GetContentsByIndate("item", inDate).GetReturnValuetoJSON()["row"][0]["itemDict"]["M"];
+        JsonData jsonData = Backend.GameInfo.GetContentsByIndate("item", inDate).GetReturnValuetoJSON()["row"][0]["items"]["M"];
         Dictionary<string, int> dict = GetDictFromJsonData(jsonData);
         dict[itemName] = number;
 
         Param param = new Param();
-        param.Add("itemDict", dict);
+        param.Add("items", dict);
 
         Backend.GameInfo.Update("item", inDate, param);
     }
 
-    private void DeleteItem(string inDate, string itemName, int number)
+    private void DeleteItem(string inDate, string itemName)
     {
-        JsonData jsonData = Backend.GameInfo.GetContentsByIndate("item", inDate).GetReturnValuetoJSON()["row"][0]["itemDict"]["M"];
+        JsonData jsonData = Backend.GameInfo.GetContentsByIndate("item", inDate).GetReturnValuetoJSON()["row"][0]["items"]["M"];
         Dictionary<string, int> dict = GetDictFromJsonData(jsonData);
         dict.Remove(itemName);
 
         Param param = new Param();
-        param.Add("itemDict", dict);
+        param.Add("items", dict);
 
         Backend.GameInfo.Update("item", inDate, param);
     }
@@ -200,6 +198,359 @@ public class BackendManager : MonoBehaviour
     #endregion
 
     #region Table : town
+
+    private void CreateTown(string playerId)
+    {
+        Param param = new Param();
+        param.Add("playerId", playerId);
+
+        Param[] structures = new Param[0];
+        param.Add("structures", structures);
+
+        Backend.GameInfo.Insert("town", param);
+    }
+
+    private void DestroyTown(string inDate)
+    {
+        Backend.GameInfo.Delete("town", inDate);
+    }
+
+    private void AddTown(string inDate, string structureId, string buildingName, int level, int position, bool isConstructed, bool isFlipped, bool isUpgrading, DateTime endDate)
+    {
+        JsonData jsonData = Backend.GameInfo.GetContentsByIndate("town", inDate).GetReturnValuetoJSON()["row"][0]["structures"]["L"];
+        Param[] structures = GetStructuresFromJsonData(jsonData);
+
+        Param[] result = new Param[structures.Length + 1];
+        Array.Copy(structures, 0, result, 0, structures.Length);
+
+        Param newStructure = new Param();
+        newStructure.Add("structureId", structureId);
+        newStructure.Add("buildingName", buildingName);
+        newStructure.Add("level", level);
+        newStructure.Add("position", position);
+        newStructure.Add("isConstructed", isConstructed);
+        newStructure.Add("isFlipped", isFlipped);
+        newStructure.Add("isUpgrading", isUpgrading);
+        newStructure.Add("endDate", endDate.ToString());
+        result[structures.Length] = newStructure;
+
+        Param param = new Param();
+        param.Add("structures", result);
+
+        Backend.GameInfo.Update("town", inDate, param);
+    }
+
+    private void UpdateTownBuildingName(string inDate, string structureId, string buildingName)
+    {
+        JsonData jsonData = Backend.GameInfo.GetContentsByIndate("town", inDate).GetReturnValuetoJSON()["row"][0]["structures"]["L"];
+        Param[] structures = GetStructuresFromJsonData(jsonData);
+
+        SortedList[] structuresList = new SortedList[structures.Length];
+        for(int i = 0; i < structuresList.Length; i++)
+        {
+            structuresList[i] = structures[i].GetValue();
+        }
+
+        for(int i = 0; i < structures.Length; i++)
+        {
+            if (structuresList[i]["structureId"].ToString() == structureId)
+            {
+                Param newStructure = new Param();
+                newStructure.Add("structureId", structuresList[i]["structureId"].ToString());
+                newStructure.Add("buildingName", buildingName);
+                newStructure.Add("level", int.Parse(structuresList[i]["level"].ToString()));
+                newStructure.Add("position", int.Parse(structuresList[i]["position"].ToString()));
+                newStructure.Add("isConstructed", bool.Parse(structuresList[i]["isConstructed"].ToString()));
+                newStructure.Add("isFlipped", bool.Parse(structuresList[i]["isFlipped"].ToString()));
+                newStructure.Add("isUpgrading", bool.Parse(structuresList[i]["isUpgrading"].ToString()));
+                newStructure.Add("endDate", structuresList[i]["endDate"].ToString());
+
+                structures[i] = newStructure;
+            }
+        }
+
+        Param param = new Param();
+        param.Add("structures", structures);
+
+        Backend.GameInfo.Update("town", inDate, param);
+    }
+
+    private void UpdateTownLevel(string inDate, string structureId, int level)
+    {
+        JsonData jsonData = Backend.GameInfo.GetContentsByIndate("town", inDate).GetReturnValuetoJSON()["row"][0]["structures"]["L"];
+        Param[] structures = GetStructuresFromJsonData(jsonData);
+
+        SortedList[] structuresList = new SortedList[structures.Length];
+        for (int i = 0; i < structuresList.Length; i++)
+        {
+            structuresList[i] = structures[i].GetValue();
+        }
+
+        for (int i = 0; i < structures.Length; i++)
+        {
+            if (structuresList[i]["structureId"].ToString() == structureId)
+            {
+                Param newStructure = new Param();
+                newStructure.Add("structureId", structuresList[i]["structureId"].ToString());
+                newStructure.Add("buildingName", structuresList[i]["buildingName"].ToString());
+                newStructure.Add("level", level);
+                newStructure.Add("position", int.Parse(structuresList[i]["position"].ToString()));
+                newStructure.Add("isConstructed", bool.Parse(structuresList[i]["isConstructed"].ToString()));
+                newStructure.Add("isFlipped", bool.Parse(structuresList[i]["isFlipped"].ToString()));
+                newStructure.Add("isUpgrading", bool.Parse(structuresList[i]["isUpgrading"].ToString()));
+                newStructure.Add("endDate", structuresList[i]["endDate"].ToString());
+
+                structures[i] = newStructure;
+            }
+        }
+
+        Param param = new Param();
+        param.Add("structures", structures);
+
+        Backend.GameInfo.Update("town", inDate, param);
+    }
+
+    private void UpdateTownPosition(string inDate, string structureId, int position)
+    {
+        JsonData jsonData = Backend.GameInfo.GetContentsByIndate("town", inDate).GetReturnValuetoJSON()["row"][0]["structures"]["L"];
+        Param[] structures = GetStructuresFromJsonData(jsonData);
+
+        SortedList[] structuresList = new SortedList[structures.Length];
+        for (int i = 0; i < structuresList.Length; i++)
+        {
+            structuresList[i] = structures[i].GetValue();
+        }
+
+        for (int i = 0; i < structures.Length; i++)
+        {
+            if (structuresList[i]["structureId"].ToString() == structureId)
+            {
+                Param newStructure = new Param();
+                newStructure.Add("structureId", structuresList[i]["structureId"].ToString());
+                newStructure.Add("buildingName", structuresList[i]["buildingName"].ToString());
+                newStructure.Add("level", int.Parse(structuresList[i]["level"].ToString()));
+                newStructure.Add("position", position);
+                newStructure.Add("isConstructed", bool.Parse(structuresList[i]["isConstructed"].ToString()));
+                newStructure.Add("isFlipped", bool.Parse(structuresList[i]["isFlipped"].ToString()));
+                newStructure.Add("isUpgrading", bool.Parse(structuresList[i]["isUpgrading"].ToString()));
+                newStructure.Add("endDate", structuresList[i]["endDate"].ToString());
+
+                structures[i] = newStructure;
+            }
+        }
+
+        Param param = new Param();
+        param.Add("structures", structures);
+
+        Backend.GameInfo.Update("town", inDate, param);
+    }
+
+    private void UpdateTownConstructed(string inDate, string structureId, bool isConstructed)
+    {
+        JsonData jsonData = Backend.GameInfo.GetContentsByIndate("town", inDate).GetReturnValuetoJSON()["row"][0]["structures"]["L"];
+        Param[] structures = GetStructuresFromJsonData(jsonData);
+
+        SortedList[] structuresList = new SortedList[structures.Length];
+        for (int i = 0; i < structuresList.Length; i++)
+        {
+            structuresList[i] = structures[i].GetValue();
+        }
+
+        for (int i = 0; i < structures.Length; i++)
+        {
+            if (structuresList[i]["structureId"].ToString() == structureId)
+            {
+                Param newStructure = new Param();
+                newStructure.Add("structureId", structuresList[i]["structureId"].ToString());
+                newStructure.Add("buildingName", structuresList[i]["buildingName"].ToString());
+                newStructure.Add("level", int.Parse(structuresList[i]["level"].ToString()));
+                newStructure.Add("position", int.Parse(structuresList[i]["position"].ToString()));
+                newStructure.Add("isConstructed", isConstructed);
+                newStructure.Add("isFlipped", bool.Parse(structuresList[i]["isFlipped"].ToString()));
+                newStructure.Add("isUpgrading", bool.Parse(structuresList[i]["isUpgrading"].ToString()));
+                newStructure.Add("endDate", structuresList[i]["endDate"].ToString());
+
+                structures[i] = newStructure;
+            }
+        }
+
+        Param param = new Param();
+        param.Add("structures", structures);
+
+        Backend.GameInfo.Update("town", inDate, param);
+    }
+
+    private void UpdateTownFlipped(string inDate, string structureId, bool isFlipped)
+    {
+        JsonData jsonData = Backend.GameInfo.GetContentsByIndate("town", inDate).GetReturnValuetoJSON()["row"][0]["structures"]["L"];
+        Param[] structures = GetStructuresFromJsonData(jsonData);
+
+        SortedList[] structuresList = new SortedList[structures.Length];
+        for (int i = 0; i < structuresList.Length; i++)
+        {
+            structuresList[i] = structures[i].GetValue();
+        }
+
+        for (int i = 0; i < structures.Length; i++)
+        {
+            if (structuresList[i]["structureId"].ToString() == structureId)
+            {
+                Param newStructure = new Param();
+                newStructure.Add("structureId", structuresList[i]["structureId"].ToString());
+                newStructure.Add("buildingName", structuresList[i]["buildingName"].ToString());
+                newStructure.Add("level", int.Parse(structuresList[i]["level"].ToString()));
+                newStructure.Add("position", int.Parse(structuresList[i]["position"].ToString()));
+                newStructure.Add("isConstructed", bool.Parse(structuresList[i]["isConstructed"].ToString()));
+                newStructure.Add("isFlipped", isFlipped);
+                newStructure.Add("isUpgrading", bool.Parse(structuresList[i]["isUpgrading"].ToString()));
+                newStructure.Add("endDate", structuresList[i]["endDate"].ToString());
+
+                structures[i] = newStructure;
+            }
+        }
+
+        Param param = new Param();
+        param.Add("structures", structures);
+
+        Backend.GameInfo.Update("town", inDate, param);
+    }
+
+    private void UpdateTownUpgrading(string inDate, string structureId, bool isUpgrading)
+    {
+        JsonData jsonData = Backend.GameInfo.GetContentsByIndate("town", inDate).GetReturnValuetoJSON()["row"][0]["structures"]["L"];
+        Param[] structures = GetStructuresFromJsonData(jsonData);
+
+        SortedList[] structuresList = new SortedList[structures.Length];
+        for (int i = 0; i < structuresList.Length; i++)
+        {
+            structuresList[i] = structures[i].GetValue();
+        }
+
+        for (int i = 0; i < structures.Length; i++)
+        {
+            if (structuresList[i]["structureId"].ToString() == structureId)
+            {
+                Param newStructure = new Param();
+                newStructure.Add("structureId", structuresList[i]["structureId"].ToString());
+                newStructure.Add("buildingName", structuresList[i]["buildingName"].ToString());
+                newStructure.Add("level", int.Parse(structuresList[i]["level"].ToString()));
+                newStructure.Add("position", int.Parse(structuresList[i]["position"].ToString()));
+                newStructure.Add("isConstructed", bool.Parse(structuresList[i]["isConstructed"].ToString()));
+                newStructure.Add("isFlipped", bool.Parse(structuresList[i]["isFlipped"].ToString()));
+                newStructure.Add("isUpgrading", isUpgrading);
+                newStructure.Add("endDate", structuresList[i]["endDate"].ToString());
+
+                structures[i] = newStructure;
+            }
+        }
+
+        Param param = new Param();
+        param.Add("structures", structures);
+
+        Backend.GameInfo.Update("town", inDate, param);
+    }
+
+    private void UpdateTownEndDate(string inDate, string structureId, DateTime endDate)
+    {
+        JsonData jsonData = Backend.GameInfo.GetContentsByIndate("town", inDate).GetReturnValuetoJSON()["row"][0]["structures"]["L"];
+        Param[] structures = GetStructuresFromJsonData(jsonData);
+
+        SortedList[] structuresList = new SortedList[structures.Length];
+        for (int i = 0; i < structuresList.Length; i++)
+        {
+            structuresList[i] = structures[i].GetValue();
+        }
+
+        for (int i = 0; i < structures.Length; i++)
+        {
+            if (structuresList[i]["structureId"].ToString() == structureId)
+            {
+                Param newStructure = new Param();
+                newStructure.Add("structureId", structuresList[i]["structureId"].ToString());
+                newStructure.Add("buildingName", structuresList[i]["buildingName"].ToString());
+                newStructure.Add("level", int.Parse(structuresList[i]["level"].ToString()));
+                newStructure.Add("position", int.Parse(structuresList[i]["position"].ToString()));
+                newStructure.Add("isConstructed", bool.Parse(structuresList[i]["isConstructed"].ToString()));
+                newStructure.Add("isFlipped", bool.Parse(structuresList[i]["isFlipped"].ToString()));
+                newStructure.Add("isUpgrading", bool.Parse(structuresList[i]["isUpgrading"].ToString()));
+                newStructure.Add("endDate", endDate.ToString());
+
+                structures[i] = newStructure;
+            }
+        }
+
+        Param param = new Param();
+        param.Add("structures", structures);
+
+        Backend.GameInfo.Update("town", inDate, param);
+    }
+
+    private void DeleteTown(string inDate, string structureId)
+    {
+        bool isDeleted = false;
+        int count = 0;
+
+        JsonData jsonData = Backend.GameInfo.GetContentsByIndate("town", inDate).GetReturnValuetoJSON()["row"][0]["structures"]["L"];
+        Param[] structures = GetStructuresFromJsonData(jsonData);
+        Param[] result = new Param[Math.Max(structures.Length - 1, 0)];
+
+        SortedList[] structuresList = new SortedList[structures.Length];
+        for (int i = 0; i < structuresList.Length; i++)
+        {
+            structuresList[i] = structures[i].GetValue();
+        }
+
+        for(int i = 0; i < structures.Length; i++)
+        {
+            if(structuresList[i]["structureId"].ToString() != structureId)
+            {
+                Param newStructure = new Param();
+                newStructure.Add("structureId", structuresList[i]["structureId"].ToString());
+                newStructure.Add("buildingName", structuresList[i]["buildingName"].ToString());
+                newStructure.Add("level", int.Parse(structuresList[i]["level"].ToString()));
+                newStructure.Add("position", int.Parse(structuresList[i]["position"].ToString()));
+                newStructure.Add("isConstructed", bool.Parse(structuresList[i]["isConstructed"].ToString()));
+                newStructure.Add("isFlipped", bool.Parse(structuresList[i]["isFlipped"].ToString()));
+                newStructure.Add("isUpgrading", bool.Parse(structuresList[i]["isUpgrading"].ToString()));
+                newStructure.Add("endDate", structuresList[i]["endDate"].ToString());
+
+                result[count] = newStructure;
+                count++;
+            }
+            else
+            {
+                isDeleted = true;
+            }
+        }
+
+        if(isDeleted == true)
+        {
+            Param param = new Param();
+            param.Add("structures", result);
+
+            Backend.GameInfo.Update("town", inDate, param);
+        }
+    }
+
+    private Param[] GetStructuresFromJsonData(JsonData jsonData)
+    {
+        Param[] structures = new Param[jsonData.Count];
+
+        for (int i = 0; i < jsonData.Count; i++)
+        {
+            structures[i] = new Param();
+            structures[i].Add("structureId", jsonData[i]["M"]["structureId"]["S"].ToString());
+            structures[i].Add("buildingName", jsonData[i]["M"]["buildingName"]["S"].ToString());
+            structures[i].Add("level", int.Parse(jsonData[i]["M"]["level"]["N"].ToString()));
+            structures[i].Add("position", int.Parse(jsonData[i]["M"]["position"]["N"].ToString()));
+            structures[i].Add("isConstructed", bool.Parse(jsonData[i]["M"]["isConstructed"]["BOOL"].ToString()));
+            structures[i].Add("isFlipped", bool.Parse(jsonData[i]["M"]["isFlipped"]["BOOL"].ToString()));
+            structures[i].Add("isUpgrading", bool.Parse(jsonData[i]["M"]["isUpgrading"]["BOOL"].ToString()));
+            structures[i].Add("endDate", jsonData[i]["M"]["endDate"]["S"].ToString());
+        }
+
+        return structures;
+    }
 
     #endregion
 
