65370aeb43c84b1e321711772dfeedb07356215e
ironside2001 <ironside2001@naver.com>
2019-08-13 17:32:53 +0900

    [Add] 개발자 모드 버튼 추가

---
+++ Assets/Scripts/MainScene/DeveloperModeButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace AlchemyPlanet.MainScene
+{
+    public class DeveloperModeButton : MonoBehaviour
+    {
+        private Button button;
+
+        private void Awake()
+        {
+            button = GetComponent<Button>();
+            button.onClick.AddListener(() =>
+            {
+                Data.PlayGamesScript.Instance.isDeveloperMode = !Data.PlayGamesScript.Instance.isDeveloperMode;
+            });
+        }
+    }
+}
